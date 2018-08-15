c3164689167db354f305d09de9b8107a779e0866
Luis Mesa <luismesagravedeperalta@gmail.com>
2018-08-15 13:09:05 +0200

    - run migratios at startup

--- Joker.Clipboard.WebUI/Program.cs
+++ Joker.Clipboard.WebUI/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Joker.Clipboard.WebUI.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Joker.Clipboard.WebUI
@@ -14,12 +17,28 @@ namespace Joker.Clipboard.WebUI
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            RunMigrations(host);
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+
+
+        private static void RunMigrations(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+        }
+
     }
 }
